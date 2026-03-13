39641e58802ec964a826e80763fc0fa49eda3361
SayanCispl <sayan.koley@codeclouds.co.in>
2026-03-13 19:35:01 +0530

    Fix workflow and test setup for Playwright CI

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import Page
 from config.config import Config
 
+
 class CartPage:
     def __init__(self, page: Page):
         self.page = page
@@ -11,7 +12,19 @@ class CartPage:
         self.page.goto(Config.SHOP_URL)
 
     def add_item_to_cart(self):
-        self.page.click(self.add_to_cart_button)
+        # Locate the button
+        add_to_cart = self.page.locator(self.add_to_cart_button)
+
+        # Hover to reveal the overlay (triggers the hover effect)
+        add_to_cart.hover()
+
+        # Wait for hover animation to complete (300ms is usually enough)
+        self.page.wait_for_timeout(300)
+
+        # Now click the button
+        add_to_cart.click()
+
+        # Rest of the flow
         self.page.wait_for_selector(self.view_cart_button)
         self.page.click(self.view_cart_button)
 
